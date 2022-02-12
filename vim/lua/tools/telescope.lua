cc25c0520823403d3434a4646fe60a90ce355afb
tonyyu <tonyyu@indeed.com>
2022-02-12 15:40:33 -0600

    feat: Configure telescope buffers to sort by MRU

--- vim/lua/tools/telescope.lua
+++ vim/lua/tools/telescope.lua
@@ -1,4 +1,10 @@
 local telescope = require('telescope')
 
-telescope.setup()
+telescope.setup{
+    pickers = {
+        buffers = {
+          sort_mru = true,
+        },
+    },
+}
 telescope.load_extension('fzf')
