9fd26f067a2b3725ab3894c217910a5bbc7457f4
tonyyu <tonyyu@indeed.com>
2022-02-12 21:21:59 -0600

    refactor: Simplify normal mapping definition

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -6,28 +6,28 @@ local lsp_installer = require('nvim-lsp-installer')
 -- Attach keymappings to LSP servers
 -- See https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
 local function on_attach(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function normal_map(...) vim.api.nvim_buf_set_keymap(bufnr, 'n', ...) end
 
     local opts = { noremap=true, silent=true }
 
     -- Definitions and references
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    normal_map('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    normal_map('gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    normal_map('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    normal_map('gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    normal_map('<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 
     -- Documentation
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    normal_map('K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    normal_map('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
     -- Diagnostics
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    normal_map('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    normal_map(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 
     -- Editing
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    normal_map('<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    normal_map('<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 lsp_installer.on_server_ready(function(server)
