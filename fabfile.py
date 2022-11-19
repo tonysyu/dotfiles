4510d47ac92eb4e4b0c8606575ed69c3f055173a
Tony S Yu <tsyu80@gmail.com>
2022-11-18 22:31:31 -0600

    Remove really old fabfile for updating vim bundles

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabric.api import task, local
-
-
-@task
-def update_vim():
-    """ Update all vim bundles. """
-    # `|| :` skips on error instead of terminating
-    cmd = "git submodule foreach 'git pull origin master || :'"
-    local(cmd)
