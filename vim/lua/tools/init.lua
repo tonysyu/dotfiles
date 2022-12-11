3c1fd64611bb24ba8a84fac90465a6d08cba2dca
Tony S Yu <tsyu80@gmail.com>
2022-12-10 20:50:00 -0600

    Fix symbols-outline and consolidate simple setups

--- vim/lua/tools/init.lua
+++ vim/lua/tools/init.lua
@@ -1,9 +1,14 @@
 require('tools.buftabline')
 require('tools.closetag')
-require('tools.devicons')
 require('tools.emmet')
 require('tools.indent-blankline')
 require('tools.lightline')
 require('tools.surround')
 require('tools.telescope')
 require('tools.ultisnips')
+
+-- Basic setup scripts
+-- Only include _very_ basic tool setup. Customizations should go in separate files
+require('nvim-web-devicons').setup()
+require("symbols-outline").setup()
+require("trouble").setup {}
