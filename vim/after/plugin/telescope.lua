8191bb332dc05093da56e6c1742a548870e8f90a
tonyyu <tonyyu@indeed.com>
2022-12-22 11:39:49 -0600

    Add default treesitter parsers

--- vim/after/plugin/telescope.lua
+++ vim/after/plugin/telescope.lua
@@ -1,17 +1,37 @@
 local telescope = require('telescope')
 
 telescope.setup{
-	defaults = {
+    ensure_installed = {
+        'graphql',
+        'html',
+        'java',
+        'javascript',
+        'json',
+        'kotlin',
+        'lua',
+        'make',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'rst',
+        'scss',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'yaml',
+    },
+    defaults = {
         path_display = {
             shorten = {
                 -- Keep the first and last-three path parts unshortened
                 exclude = {1, -3, -2, -1},
             },
         },
-	},
+    },
     pickers = {
         buffers = {
-          sort_mru = true,
+            sort_mru = true,
         },
     },
 }
