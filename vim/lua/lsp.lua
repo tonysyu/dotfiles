80ee02b552de280425b5d5b0cbf31111a6e801cf
Tony S Yu <tsyu80@gmail.com>
2023-01-02 22:01:37 -0600

    Fix completion configuration
    Previous config displayed completion that changed after starting
    selection.

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -83,19 +83,24 @@ mason_lspconfig.setup_handlers({
     end
 })
 
+local luasnip = require 'luasnip'
+
 cmp.setup({
     snippet = {
         expand = function(args)
-            vim.fn["UltiSnips#Anon"](args.body)
+            luasnip.lsp_expand(args.body)
         end,
     },
-    mapping = {
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
     sources = {
         { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
-        { name = 'buffer' },
-        { name = "ultisnips" },
+        { name = "luasnip" },
     }
 })
--- vim/lua/tools/ultisnips.lua
+++
@@ -1,4 +0,0 @@
-vim.g.UltiSnipsExpandTrigger       = "<c-j>"
-vim.g.UltiSnipsJumpForwardTrigger  = "<c-j>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<c-p>"
-vim.g.UltiSnipsListSnippets        = "<c-k>" -- List possible snippets based on current file
