bb69c739da0b64c56149892d3317ea091486fdfd
Tony S Yu <tsyu80@gmail.com>
2024-08-27 22:37:40 -0500

    vim: Add auto-save auto command

--- vim/after/plugin/general-editing.lua
+++ vim/after/plugin/general-editing.lua
@@ -1,14 +1,23 @@
 -- Enable Comment.nvim
 require('Comment').setup()
 
+local default_augroup = vim.api.nvim_create_augroup('GeneralEditingAuGroup', { clear = true })
+
+-- Auto-save when leaving
+-- ..........................................................................
+vim.api.nvim_create_autocmd('BufLeave', {
+    command = 'wall',
+    group = default_augroup,
+    pattern = '*',
+})
+
 -- Highlight on yank
 -- ..........................................................................
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank()
     end,
-    group = highlight_group,
+    group = default_augroup,
     pattern = '*',
 })
