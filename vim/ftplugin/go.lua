dd8616792f6b4232a74ed6f4b046ce0f05067348
Tony S Yu <tsyu80@gmail.com>
2024-09-02 20:48:42 -0500

    vim: Avoid converting tabs to spaces for go files

--- vim/ftplugin/go.lua
+++ vim/ftplugin/go.lua
@@ -2,3 +2,6 @@ local opt = vim.opt
 
 -- Set textwidth to 100 to match default used by https://github.com/segmentio/golines
 opt.textwidth = 100
+
+-- Go best practice is tabs, not spaces
+opt.expandtab = false
