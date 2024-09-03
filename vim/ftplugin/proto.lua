f5a060636794f42e653e2886e3c4b5d9f578dc48
Tony S Yu <tsyu80@gmail.com>
2024-09-02 20:50:16 -0500

    vim: Configure defaults for proto files

---
+++ vim/ftplugin/proto.lua
@@ -0,0 +1,10 @@
+local opt = vim.opt
+
+-- Set textwidth to 80 to match recommendation.
+opt.textwidth = 80
+
+-- Indentation
+-- ............................................................................
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
