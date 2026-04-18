09627140e320ff08bb0a01c226232ddba661c791
Tony S Yu <tsyu80@gmail.com>
2026-04-17 22:42:08 -0500

    feat(vim): Add cfilter package for quickfix filtering

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,6 +7,11 @@ require('lsp')
 require('treesitter')
 require('custom_commands')
 
+-- Add Cfilter command to filter for or out (!) lines of quickfix window
+-- Use w/ delete line (dd) keymap defined in after/plugin/general-editing
+-- :Cfilter[!] /{pat}/
+vim.cmd('packadd cfilter')
+
 -- Old vimscript files not-yet converted to lua
 local function source_vimscript(path)
   vim.cmd('source ' .. path)
