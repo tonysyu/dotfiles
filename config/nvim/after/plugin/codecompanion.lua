c10fa21411ba0d010fc0c01b4562a7e4e6f153f4
Tony S Yu <tsyu80@gmail.com>
2026-05-02 23:44:37 -0500

    feat(vim): Update CodeCompanion keymaps

--- config/nvim/after/plugin/codecompanion.lua
+++ config/nvim/after/plugin/codecompanion.lua
@@ -36,13 +36,25 @@ vim.cmd([[cab cc CodeCompanion]])
 -- Expand 'ccc' into 'CodeCompanionCLI' in the command line
 vim.cmd([[cab ccc CodeCompanionCLI]])
 
--- Toggle CodeCompanionCLI. Create session, if not already created.
-vim.keymap.set({ "n", "v" }, "<leader>ccc", function()
+-- Toggle CodeCompanionCLI (supports two separate keymappings to trigger)
+local cc_cli_opts = { noremap = true, silent = true, desc = "AI: Toggle CodeCompanion CLI" }
+local cc_cli_toggle_fn = function()
   require("codecompanion.interactions.cli").toggle()
-end, { noremap = true, silent = true, desc = "AI: Toggle CodeCompanion CLI" })
+end
+vim.keymap.set({ "n", "v" }, "<leader>ccc", cc_cli_toggle_fn, cc_cli_opts)
+vim.keymap.set({ "n", "v" }, "<C-A-/>", cc_cli_toggle_fn, cc_cli_opts)
+
+-- Prompt CodeCompanionCLI from within the editor
 vim.keymap.set({ "n", "v" }, "<leader>ccp", function()
   return require("codecompanion").cli({ prompt = true })
-end, { desc = "AI: Prompt the CLI agent" })
+end, { desc = "AI: Prompt the CLI agent (submit using ⌃⏎)" })
+
+-- Add focused context to CodeCompanionCLI
 vim.keymap.set({ "n", "v" }, "<leader>cca", function()
   return require("codecompanion").cli("#{this}")
 end, { desc = "AI: Add context to the CLI agent" })
+
+-- Add current file in CodeCompanionCLI from within the editor
+vim.keymap.set({ "n", "v" }, "<leader>ccf", function()
+  return require("codecompanion").cli("#{buffer}")
+end, { desc = "AI: Add file to the CLI agent" })
